a68090e043f7614fdcfa0a5877ea49053038d2d9
lucas-olmelo <lucaas1904@gmail.com>
2023-10-09 16:00:02 -0300

    Novos avisos e tela de contato

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CartService } from "../../services/cart.service";
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-dropdown',
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.css']
 })
+
 export class DropdownComponent {
 
   constructor(private cartService: CartService){ }
@@ -76,9 +78,37 @@ export class DropdownComponent {
   }
   
   clickButton(){
-    alert(this.game.nome + ' adicionado ao carrinho')
+    this.mostraDialogo(this.game.nome);
     this.cartService.adicionaLista(this.gameCovers.filter((x)=> x.id == this.select1)[0]);
     this.game = {id: 0, nome: '', preco: 0, img: ''};
     this.select1 = 99;
+  }
+
+  mostraDialogo(gameName: string) {
+
+    // monta o css da mensagem para que fique flutuando na frente de todos elementos da página
+    var cssMessage = "display: block; position: fixed; bottom: 5%; left: 20%; right: 20%; width: 60%; padding-top: 10px; z-index: 9999";
+    var cssInner = "padding: 2%; font-size: 18px; margin: 0 auto; box-shadow: 1px 1px 5px black;";
+
+    // monta o html da mensagem com Bootstrap
+    var dialogo = "";
+    dialogo += '<div id="message" style="'+cssMessage+'">';
+    dialogo += '    <div class="alert alert-success alert-dismissable" style="'+cssInner+'">';
+    dialogo += '    <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>';
+    dialogo += `    ${gameName} foi adicionado ao carrinho`
+    dialogo += '    </div>';
+    dialogo += '</div>';
+
+    // adiciona ao body a mensagem com o efeito de fade
+    $("body").append(dialogo);
+    $("#message").hide();
+    $("#message").fadeIn(200);
+
+    // contador de tempo para a mensagem sumir
+    setTimeout(function() {
+      $('#message').fadeOut(300, function(){
+          $(this).remove();
+      });
+    }, 3000); // milliseconds
   }
 }
--- src/app/pages/fale-conosco/fale-conosco.component.ts
+++ src/app/pages/fale-conosco/fale-conosco.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-fale-conosco',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class FaleConoscoComponent {
 
+  mostraDialogo() {
+
+    // monta o css da mensagem para que fique flutuando na frente de todos elementos da página
+    var cssMessage = "display: block; position: fixed; bottom: 5%; left: 20%; right: 20%; width: 60%; padding-top: 10px; z-index: 9999";
+    var cssInner = "padding: 2%; font-size: 18px; margin: 0 auto; box-shadow: 1px 1px 5px black;";
+
+    // monta o html da mensagem com Bootstrap
+    var dialogo = "";
+    dialogo += '<div id="message" style="'+cssMessage+'">';
+    dialogo += '    <div class="alert alert-success alert-dismissable" style="'+cssInner+'">';
+    dialogo += '    <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>';
+    dialogo += `    Mensagem enviada com sucesso!`
+    dialogo += '    </div>';
+    dialogo += '</div>';
+
+    // adiciona ao body a mensagem com o efeito de fade
+    $("body").append(dialogo);
+    $("#message").hide();
+    $("#message").fadeIn(200);
+
+    // contador de tempo para a mensagem sumir
+    setTimeout(function() {
+      $('#message').fadeOut(300, function(){
+          $(this).remove();
+      });
+    }, 3000); // milliseconds
+  }
 }
