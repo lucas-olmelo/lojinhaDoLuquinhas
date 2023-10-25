dc49080417c91be90bd1d86d2b59bd101243900a
Lucas Melo <lucaas1904@gmail.com>
2023-10-24 21:47:19 -0300

    Fixes

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
-import * as $ from 'jquery';
 
 @Component({
   selector: 'app-product',
@@ -10,10 +9,6 @@ import * as $ from 'jquery';
 export class ProductComponent {
 
   constructor(private cartService: CartService) {}
-
-  ngOnInit() {
-    this.checkItem()
-  }
 
   wishList: {id: number, nome: string, preco: number, img: string}[] = [];
 
@@ -45,34 +40,5 @@ export class ProductComponent {
       heart?.classList.remove('noDisplay');
       this.cartService.removeListaDesejo(this.game);
     }
-  }
-
-  checkItem() {
-
-    // var icon = '';
-    // if (!this.cartService.retornaLista().includes(this.game)) {
-    //   console.log('oi');
-      
-    //   icon += '<span class="btnHeart'+this.game.id+'">'
-    //   icon += '<i (click)="clickHeart' +this.game.id +'" class="bi bi-heart" style="font-size: 30px; color: black;"></i>'
-    //   icon += '</span>';
-    // } else {
-    //   console.log('txhau');
-
-    //   icon += '<span class="btnHeartFill{{game.id}}">'
-    //   icon += '<i (click)="clickHeart(game.id)" class="bi bi-heart-fill" style="font-size: 30px; color: black;"></i>'
-    //   icon += '</span>';
-    // }
-
-    // let id = this.game.id
-
-    // let queryString = `btnHeart${id}`;
-    
-    // var test = document.querySelectorAll('#btnHeart');
-    // test[this.game.id].appendChild(icon)
-    // console.log(test);
-    
-    // $('#btnHeart'+id).append(icon);
-
   }
 }
